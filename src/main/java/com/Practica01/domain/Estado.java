72981b8baa648c56baa923acdf8c2c7932f25ee9
fabricio <furimatarrita15@gmail.com>
2023-02-28 15:12:19 -0600

    Correciones de BD y templates

--- src/main/java/com/Practica01/domain/Estado.java
+++ src/main/java/com/Practica01/domain/Estado.java
@@ -6,31 +6,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.io.Serializable;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name="estado")
-public class Estado {
-    private static final Long serialVersionUID = 1L;
+public class Estado implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id_estado;
-    String capital;
-    String poblacion;
-    String idioma;
-    String codigoPais;
-    String moneda;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEstado;
+    private String capital;
+    private String poblacion;
+    private String idioma;
+    private String codigo;
+    private String moneda;
 
     public Estado() {
     }
 
-    public Estado(Long id_estado, String capital, String poblacion, String idioma, String codigoPais, String moneda) {
-        this.id_estado = id_estado;
+    public Estado(String capital, String poblacion, String idioma, String codigo, String moneda) {
         this.capital = capital;
         this.poblacion = poblacion;
         this.idioma = idioma;
-        this.codigoPais = codigoPais;
+        this.codigo = codigo;
         this.moneda = moneda;
     }
     
--- src/main/java/com/Practica01/service/EstadoServiceImpl.java
+++ src/main/java/com/Practica01/service/EstadoServiceImpl.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class EstadoServiceImpl implements EstadoService {
     
     @Autowired
-    EstadoDao estadoDao;
+    private EstadoDao estadoDao;
     
     @Override
     @Transactional(readOnly = true)
@@ -40,6 +40,6 @@ public class EstadoServiceImpl implements EstadoService {
     @Override
     @Transactional(readOnly = true)
     public Estado getEstado(Estado estado) {
-        return estadoDao.findById(estado.getId_estado()).orElse(null);
+        return estadoDao.findById(estado.getIdEstado()).orElse(null);
     } 
 }
